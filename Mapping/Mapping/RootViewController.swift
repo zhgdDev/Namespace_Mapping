4690b70fab041f59543a466f02d308d6083c0e5e
Dubai <zhgdDev@gmail.com>
2019-01-20 12:48:07 +0800

    creat Mapping project

---
+++ Mapping/Mapping/RootViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RootViewController.swift
+//  Mapping
+//
+//  Created by Dubai on 2019/1/20.
+//  Copyright © 2019 hhcu. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
